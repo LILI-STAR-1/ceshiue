5e6a75b224b7fa5e532d7580efbc1ffb6745ca71
Litonghui <120202766@qq.com>
2025-08-03 22:00:37 +0800

    初始提交

---
+++ Source/chajian/chajianGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "chajianGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AchajianGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AchajianGameMode();
+};
+
+
+
